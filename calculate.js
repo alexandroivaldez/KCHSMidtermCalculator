fe0668b1dbf1691ebbd23437ffee7dd8fa07331e
Alexandro Valdez <alexandroivaldez@gmail.com>
2022-12-02 09:19:09 -0600

    Updated error checking for missed input.
    I should've used a form and required in the html, but this was a faster fix.

--- calculate.js
+++ calculate.js
@@ -2,7 +2,12 @@ function calculate() {
   var q1 = document.getElementById("q1").value;
   var q2 = document.getElementById("q2").value;
   var dG = document.getElementById("dG").value;
-  var neededGrade = (dG - (0.425 * q1 + 0.425 * q2)) / 0.15;
-  document.getElementById("neededGrade").innerHTML =
-    "Grade Needed On Midterm: " + neededGrade.toPrecision(5) + "%";
+  
+  if(dG == "" || q1 == "" || q2 == ""){
+     document.getElementById("neededGrade").innerHTML = "Please fill in all input fields above!";
+  } else {
+    var neededGrade = (dG - (0.425 * q1 + 0.425 * q2)) / 0.15;
+    document.getElementById("neededGrade").innerHTML = "Grade Needed On Midterm: " + neededGrade.toPrecision(5) + "%";
+  }
+   
 }
